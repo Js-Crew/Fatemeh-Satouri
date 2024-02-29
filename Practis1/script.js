bc295cb863c66d065c2b72309d6d19322800dabc
Tabasoume <145778331+Tboumpsh@users.noreply.github.com>
2024-02-29 12:33:00 +0330

    [add event submit]-event for form
    Description:
    add event submit for form because when click button submit form is refresh
    + by arrow function and event object

--- Practis1/script.js
+++ Practis1/script.js
@@ -2,3 +2,12 @@
 let form = document.getElementById('form')
 let textInput = document.getElementById('text')
 let btnInput = document.getElementById('button')
+
+
+// add event submit for form because when click button submit form is refresh 
+
+form.addEventListener('submit' , e => {
+    // add event object
+    // Prevent the default form of form
+    e.preventDefault();
+})
