c93c2a33c450d7c4e4032f01efc8fb0074f59561
Tabasoume <145778331+Tboumpsh@users.noreply.github.com>
2024-02-29 12:58:17 +0330

    [create li]-create li when click button
    Description:
    create li by CreateLi function, when click the submit button li is create
    +
    catch input value

--- Practis1/script.js
+++ Practis1/script.js
@@ -1,27 +1,33 @@
 // select all element in js
-let form = document.getElementById('form')
-let textInput = document.getElementById('text')
-let btnInput = document.getElementById('button')
+let form = document.getElementById("form");
+let textInput = document.getElementById("text");
+let btnInput = document.getElementById("button");
 
+// add event submit for form because when click button submit form is refresh
 
-// add event submit for form because when click button submit form is refresh 
-
-form.addEventListener('submit' , e => {
-    // add event object
-    // Prevent the default form of form
-    e.preventDefault();
-})
+form.addEventListener("submit", (e) => {
+  // add event object
+  // Prevent the default form of form
+  e.preventDefault();
+});
 
 // add click event for button
-btnInput.addEventListener('click' , clickButton)
-function clickButton()
-{
-
+btnInput.addEventListener("click", clickButton);
+function clickButton() {
+  // select input value
+  let text = textInput.value;
+  createLi(text);
 }
-
 
 // create li function
-function createLi(text)
-{
-
+function createLi(text) {
+  let li = document.createElement("li");
+  let span = document.createElement("span");
+  span.innerHTML = text;
+  let a = document.createElement("a");
+  a.textContent = "X";
+  a.classList.add("delete");
+  span.append(a);
+  li.append(span);
 }
+// createLi('jdj')
